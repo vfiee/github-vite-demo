4b87994012f73b4274dff5b931105e715c8f1b35
vyron <fiee_vyron@163.com>
2022-10-04 23:15:29 +0800

    style: 修改颜色

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">count is <span class="count">{{ count }}</span></button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -36,5 +36,8 @@ const count = ref(0)
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.count {
+  color: yellow;
 }
 </style>
